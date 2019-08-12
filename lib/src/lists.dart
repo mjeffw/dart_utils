832ffdb42e8522fe1b0e100d439affc8f12078d0
Wilson, Marvin (jw9615) <jw9615@att.com>
2019-08-12 15:50:53 -0400

    remove listEquals -- depend on Quiver's impl

--- lib/src/lists.dart
+++ lib/src/lists.dart
@@ -3,14 +3,14 @@ class ListEx {
   /// Regrettable that we have to do this because this project is pure Dart.
   /// Flutter has a collection utility package with this method.
   ///
-  static bool listEquals(List<dynamic> one, List<dynamic> other) {
-    if (identical(one, other)) return true;
-    if (one.runtimeType != other.runtimeType || one.length != other.length) {
-      return false;
-    }
-    for (var i = 0; i < one.length; i++) {
-      if (one[i] != other[i]) return false;
-    }
-    return true;
-  }
+  // static bool listEquals(List<dynamic> one, List<dynamic> other) {
+  //   if (identical(one, other)) return true;
+  //   if (one.runtimeType != other.runtimeType || one.length != other.length) {
+  //     return false;
+  //   }
+  //   for (var i = 0; i < one.length; i++) {
+  //     if (one[i] != other[i]) return false;
+  //   }
+  //   return true;
+  // }
 }

1179c576c374329b6f65fbc92537fe0a183524b0
Wilson, Marvin (jw9615) <jw9615@att.com>
2019-08-12 19:20:16 -0400

    clean up code

--- lib/src/jsons.dart
+++ lib/src/jsons.dart
@@ -1,5 +1,4 @@
 class JsonEx {
-  static List<String> toListOfStrings(List<dynamic> json) {
-    return json.map((it) => it.toString()).toList();
-  }
+  static List<String> toListOfStrings(List<dynamic> json) =>
+      json == null ? null : json.map((it) => it.toString()).toList();
 }

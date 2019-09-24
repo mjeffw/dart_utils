6871e04fb823d0b2f9f3b35932cd830a096b0b45
Wilson, Marvin (jw9615) <jw9615@att.com>
2019-09-24 14:00:46 -0400

    change toListOfStrings to return empty list if input is null

--- lib/src/jsons.dart
+++ lib/src/jsons.dart
@@ -1,4 +1,4 @@
 class JsonEx {
   static List<String> toListOfStrings(List<dynamic> json) =>
-      json == null ? null : json.map((it) => it.toString()).toList();
+      json == null ? [] : json.map((it) => it.toString()).toList();
 }
